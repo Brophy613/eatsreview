415bb99604e21b96b110516e96a622d0226cafec
eddie <efbrophy@quinnipiac.edu>
2015-12-03 22:11:21 +0000

    Add question create view

--- core/models.py
+++ core/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+  restaurant = models.CharField(max_length=300)
+  order = models.TextField(null=True, blank=True)
+  review = models.TextField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  user = models.ForeignKey(User)
+
+  def __unicode__(self):
+    return self.title
