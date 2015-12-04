92e78034adf3282a5659bb9e2628bf855af3b3f4
eddie <efbrophy@quinnipiac.edu>
2015-12-04 21:38:06 +0000

    Add question detail view

--- core/views.py
+++ core/views.py
@@ -3,16 +3,26 @@ from django.views.generic import TemplateView
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 class Home(TemplateView):
   template_name = "home.html"
+
 class QuestionCreateView(CreateView):
     model = Question
     template_name = "question/question_form.html"
     fields = ['restaurant', 'order', 'review']
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('question_list')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(QuestionCreateView, self).form_valid(form)
+
+class QuestionListView(ListView):
+    model = Question
+    template_name = "question/question_list.html"
+
+class QuestionDetailView(DetailView):
+    model = Question
+    template_name = 'question/question_detail.html'
