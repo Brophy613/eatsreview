777cda701442b8d0473f2cb220816b6a2d7b488a
eddie <efbrophy@quinnipiac.edu>
2015-12-04 21:49:12 +0000

    Add delete view

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
-from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 
 # Create your views here.
 class Home(TemplateView):
@@ -31,3 +31,8 @@ class QuestionUpdateView(UpdateView):
     model = Question
     template_name = 'question/question_form.html'
     fields = ['restaurant', 'order', 'review']
+
+class QuestionDeleteView(DeleteView):
+    model = Question
+    template_name = 'question/question_confirm_delete.html'
+    success_url = reverse_lazy('question_list')
