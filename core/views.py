18b4b8865c99ffcefdef2efb6a986e39a05acac4
eddie <efbrophy@quinnipiac.edu>
2015-12-05 19:09:24 +0000

    user authentication

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic import CreateView
@@ -38,11 +39,21 @@ class QuestionUpdateView(UpdateView):
     model = Question
     template_name = 'question/question_form.html'
     fields = ['restaurant', 'order', 'review']
+    def get_object(self, *args, **kwargs):
+        object = super(QuestionUpdateView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
 
 class QuestionDeleteView(DeleteView):
     model = Question
     template_name = 'question/question_confirm_delete.html'
     success_url = reverse_lazy('question_list')
+    def get_object(self, *args, **kwargs):
+        object = super(QuestionDeleteView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
 
 class AnswerCreateView(CreateView):
     model = Answer
@@ -65,11 +76,21 @@ class AnswerUpdateView(UpdateView):
 
     def get_success_url(self):
         return self.object.question.get_absolute_url()
+    def get_object(self, *args, **kwargs):
+        object = super(AnswerUpdateView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
 
 class AnswerDeleteView(DeleteView):
     model = Answer
     pk_url_kwarg = 'answer_pk'
     template_name = 'answer/answer_confirm_delete.html'
+    def get_object(self, *args, **kwargs):
+        object = super(AnswerDeleteView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
 
     def get_success_url(self):
         return self.object.question.get_absolute_url()
